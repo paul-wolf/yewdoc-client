1fa4608c2f64ecc74efb7ab9c4149953f71517b0
Chris Wolf <cw10025@gmail.com>
2017-02-13 22:14:39 -0500

    Change setup.py to read requirements from requirements.txt rather then a redundant array that must be kept in-sync with requirements.txt.
    Be more flexible with pandoc and wheel version dependencies.

--- setup.py
+++ setup.py
@@ -1,24 +1,14 @@
 from setuptools import setup
 
+install_requires =[]
+with open('requirements.txt') as fh:
+    [install_requires.append(r.strip()) for r in fh if len(r.strip()) >0]
+    
 setup(
     name='yd',
     version='0.1.0',
     py_modules=['yewdoc'],
-    install_requires=[
-        "click==6.6",
-        "Jinja2==2.7.3",
-        "Markdown==2.6.2",
-        "MarkupSafe==0.23",
-        "pypandoc==0.9.9",
-        "python-dateutil==2.4.2",
-        "pytz==2015.4",
-        "requests==2.7.0",
-        "six==1.9.0",
-        "StringGenerator==0.1.9",
-        "tzlocal==1.2",
-        "wheel==0.24.0",
-        "humanize==0.5.1",
-   ],
+    install_requires=install_requires,
     entry_points='''
         [console_scripts]
         yd = yewdoc:cli
