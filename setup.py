4472de9d217a5deafbb26ef07b91cd3250aa6655
Paul Wolf <paul@bitposter.co>
2021-02-20 11:05:30 +0100

    Fixing conflict for Chris' pr

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+install_requires =[]
+with open('requirements.txt') as fh:
+    [install_requires.append(r.strip()) for r in fh if len(r.strip()) >0]
+    
 setup(
+<<<<<<< HEAD
     name="yd",
     version="0.2.0",
     # py_modules=["yewdoc"],
@@ -22,6 +27,13 @@ setup(
         "glom",
     ],
     entry_points="""
+=======
+    name='yd',
+    version='0.1.0',
+    py_modules=['yewdoc'],
+    install_requires=install_requires,
+    entry_points='''
+>>>>>>> Change setup.py to read requirements from requirements.txt rather then a redundant array that must be kept in-sync with requirements.txt.
         [console_scripts]
         yd = yd:cli
     """,
